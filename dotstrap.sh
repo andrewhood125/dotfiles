cdf19dcc1e91b80487c3a7fd086701267e1af930
Andrew Hood <andrewhood125@gmail.com>
2015-02-02 18:33:49 -0600

    add all the plugins!

--- dotstrap.sh
+++ dotstrap.sh
@@ -1,1 +1,1 @@
-deps=(vim zsh curl oh-my-zsh ruby ack ctags janus janus_vim_airline)
+deps=(vim zsh curl oh-my-zsh ruby ack ctags janus janus_vim_airline janus_vim_autoformat janus_vim_blade janus_vim_emmet janus_vim_html5 janus_vim_surround)
