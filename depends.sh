3a0b5d8d517f62f762537266cbe335d8f3160176
Andrew Hood <andrewhood125@gmail.com>
2015-04-07 22:40:49 -0500

    Moved the contents of `install.sh` into _after()

--- depends.sh
+++ depends.sh
@@ -1,1 +1,9 @@
 deps=(oh_my_zsh homestead janus janus_vim_airline janus_vim_autoformat janus_vim_blade janus_vim_emmet janus_vim_html5 janus_vim_surround zsh_syntax_highlighting)
+
+_after() {
+  for i in `ls dots`; do
+    cp -i dots/$i ~/.$i
+  done
+
+  chsh -s /bin/zsh
+}
