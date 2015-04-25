d27b85a9e8425dfc5cac7ce34e9e4d025c7a1b03
Andrew Hood <andrewhood125@gmail.com>
2015-04-25 11:35:11 -0500

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -1,10 +1,17 @@
-deps=(homestead vim-nerdcommenter vim-nerdtree vim-supertab vim-sensible vim-xoria256 vim-ctrlp zsh-syntax-highlighting)
+deps=(homestead vim-nerdcommenter vim-ruby vim-syntastic vim-nerdtree vim-supertab vim-sensible vim-xoria256 vim-ctrlp zsh-syntax-highlighting)
 
 _after() {
   for i in `ls dots`; do
     colordiff ~/.$i dots/$i 
-    cp -i dots/$i ~/.$i
+
+    # Only prompt if there is a diff in files
+    if [ "$?" -eq 1 ] ; then
+      cp -i dots/$i ~/.$i
+    fi
+
   done
 
-  chsh -s /bin/zsh
+  if [ "$SHELL" != "/bin/zsh" ] ; then
+    chsh -s /bin/zsh
+  fi
 }
