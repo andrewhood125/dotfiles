0eccff4d086895317850aed0014bdeb67be02015
Andrew Hood <andrewhood125@gmail.com>
2015-04-24 21:51:12 -0500

    Add pathogen and a few vim plugins to deps

--- depends.sh
+++ depends.sh
@@ -1,4 +1,4 @@
-deps=(oh-my-zsh homestead zsh-syntax-highlighting)
+deps=(oh-my-zsh homestead pathogen vim-sensible vim-nerdtree vim-ctrlp zsh-syntax-highlighting)
 
 _after() {
   for i in `ls dots`; do
