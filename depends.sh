e64138f69b0212af36d1b1d6cb016e695595a9d8
Andrew Hood <andrewhood125@gmail.com>
2015-05-24 23:07:56 -0700

    Add vim-ack / fix whitespace

--- depends.sh
+++ depends.sh
@@ -1,8 +1,8 @@
-deps=(homestead ctags vim-trailing-whitespace vim-delimitmate vim-airline vim-snippets vim-scala vim-blade vim-emmet vim-autoformat vim-nerdcommenter vim-ruby vim-syntastic vim-nerdtree vim-supertab vim-sensible vim-xoria256 vim-ctrlp zsh-syntax-highlighting colordiff)
+deps=(homestead ctags vim-ack vim-trailing-whitespace vim-delimitmate vim-airline vim-snippets vim-scala vim-blade vim-emmet vim-autoformat vim-nerdcommenter vim-ruby vim-syntastic vim-nerdtree vim-supertab vim-sensible vim-xoria256 vim-ctrlp zsh-syntax-highlighting colordiff)
 
 _after() {
   for i in `ls dots`; do
-    colordiff ~/.$i dots/$i 
+    colordiff ~/.$i dots/$i
 
     # Only prompt if there is a diff in files
     if [ "$?" -eq 1 ] ; then
